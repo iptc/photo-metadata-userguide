8d5b3962c50206f05c170a53f55c5b19b2d2c46e
Brendan Quinn <brendan@cluefulmedia.com>
2023-03-23 14:26:38 +0100

    Removing old versions of the spec - we can retrieve them via github if necessary

--- source_201908/asciidoctor-to-html.cmd
+++
@@ -1,5 +0,0 @@
-REM xcopy .\images ..\site\images
-
-call asciidoctor -D ..\site IPTC-PhotoMetadata-User-Guide.adoc -o index_201908.html
-
-pause
--- source_201912/asciidoctor-to-html.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Converting file... IPTC-PhotoMetadata-User-Guide.adoc"
-asciidoctor -b html5 -o ../site/index_201912.html IPTC-PhotoMetadata-User-Guide.adoc
-echo "Done."
