25932ac922f47a84ca0b98971db18cc1072414f0
Brendan Quinn <brendan@clueful.com.au>
2024-12-11 18:05:25 +0200

    Tweak to text for "illustration" example under Keywords

--- asciidoctor-to-html.sh
+++ asciidoctor-to-html.sh
@@ -2,4 +2,4 @@
 echo "Converting file... IPTC-PhotoMetadata-User-Guide.adoc"
 cp -r source/images site/
 asciidoctor -b html5 -o site/index.html source/IPTC-PhotoMetadata-User-Guide.adoc
-echo "Done."
+echo "Done. Rendered file is at site/index.html"
